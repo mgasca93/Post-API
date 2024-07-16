53c5dc2f7bbe23d51107202ac7b53a98d9579e0a
Mario Aurelio Gasca López <49210976+mgasca93@users.noreply.github.com>
2024-07-16 17:32:35 -0600

    feat(backend): add funcionality for register user api

---
+++ app/Http/Controllers/Api/V1/RegisterController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+use Symfony\Component\HttpFoundation\Response;
+use App\Http\Requests\API\V1\UserRegisterRequest;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(UserRegisterRequest $request)
+    {
+        try{
+            
+            $user = User::create([
+                'first_name'    => $request->first_name,
+                'last_name'     => $request->last_name,
+                'email'         => $request->email,
+                'password'      => Hash::make( $request->password ),
+            ]);
+
+            return response()->json([
+                'status'            => 'success',
+                'message'           => 'User created with success',
+                'data'              => $user,
+            ], Response::HTTP_CREATED);
+
+        }catch( Exception $e ){
+            return response()->json([
+                'status'        => 'failed',
+                'message'       => 'Failed to set user.'
+            ], Response::HTTP_CONFLICT);
+        }        
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
