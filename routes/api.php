4fd522155f611d8be234b1a5371ba3f0514ec6cf
Mario Aurelio Gasca López <49210976+mgasca93@users.noreply.github.com>
2024-07-16 17:31:45 -0600

    feat(backend): add new endpoint to register users to api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,19 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')->name('api.v1.')->group( function(){
+    // Route::get('/', function( Request $request ){
+    //     return response()->json([
+    //         'message' => 'Welcome to the API',
+    //         'status' => 'success',
+    //     ]);
+    // });
+
+    Route::post('/user/register', [
+            \App\Http\Controllers\API\V1\RegisterController::class, 'store'
+    ])->name('user.register');
+
+    // Route::get('/user', function (Request $request) {
+    //     return $request->user();
+    // })->middleware('auth:sanctum');
+});
