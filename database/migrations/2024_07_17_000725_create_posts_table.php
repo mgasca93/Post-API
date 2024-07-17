98c4df09a16f46f9b9526294f7e8df5fcaa3224f
Mario Aurelio Gasca López <49210976+mgasca93@users.noreply.github.com>
2024-07-16 18:50:38 -0600

    build(backend): add new migrations to api

---
+++ database/migrations/2024_07_17_000725_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string( 'name' )->nulable( false )->default( '' );
+            $table->string( 'slug' )->nulable( false )->default( '' );
+            $table->text( 'extract' )->nulable( false );
+            $table->longText( 'body' )->nulable( false );
+            $table->char( 'status', '1' )->nulable( false )->default( '1' );
+
+            $table->foreignId( 'category_id' )->constrained()->cascadeOnDelete();
+            $table->foreignId( 'user_id' )->constrained()->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
