f21c121940b95f3983873b6bd9308ad37bd56da3
Mario Aurelio Gasca López <49210976+mgasca93@users.noreply.github.com>
2024-07-17 00:30:50 -0600

    feat(backend): add new endpoint to api

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,22 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')->name('api.v1.')->group( function(){
-    // Route::get('/', function( Request $request ){
-    //     return response()->json([
-    //         'message' => 'Welcome to the API',
-    //         'status' => 'success',
-    //     ]);
-    // });
+use App\Http\Controllers\API\V1\RegisterController;
 
-    Route::post('/user/register', [
-            \App\Http\Controllers\API\V1\RegisterController::class, 'store'
-    ])->name('user.register');
+Route::prefix('v1')->name('api.v1.')->group( function(){
+
+    Route::get('/', function( Request $request ){
+        return response()->json([
+            'message' => 'Welcome to the API',
+            'status' => 'success',
+        ], Response::HTTP_OK);
+    });
+
+    Route::post('/user/register', [RegisterController::class, 'store'])->name('user.register');
+    Route::get('/user/list', [RegisterController::class, 'index']);
 
     // Route::get('/user', function (Request $request) {
     //     return $request->user();
