8d9916f55c4a0620258036061116000cff84e035
Mario Aurelio Gasca López <49210976+mgasca93@users.noreply.github.com>
2024-07-21 15:05:55 -0600

    build(backend): add category routes

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\API\V1\UserController;
+use App\Http\Controllers\API\V1\CategoryController;
 
 Route::prefix('v1')->name('api.v1.')->group( function(){
 
@@ -17,6 +18,10 @@ Route::prefix('v1')->name('api.v1.')->group( function(){
 
     Route::post('/user/create', [UserController::class, 'store'])->name('user.store');
     Route::get('/users', [UserController::class, 'index'])->name('user.index');
+
+    Route::get('/categories', [CategoryController::class, 'index'])->name('category.index');
+    Route::post('/category/create', [CategoryController::class, 'store'])->name('category.store');
+    Route::get('/category/{slug}', [CategoryController::class, 'show'])->name('category.show');
 
     // Route::get('/user', function (Request $request) {
     //     return $request->user();
